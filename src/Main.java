7bd1f2c489c1708a3f3dc604f58a31d30003568e
PLarss <per.larsson@hm.com>
2021-02-05 09:32:39 +0100

    add functionallity implemented

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,17 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+
+        int i = 0;
+
+        for (int j = 0; j < 10; j++) {
+            i = add(i);
+        }
+
+        System.out.printf("i is now: %d", i);
+    }
+
+    public static int add(int i){
+        return ++i;
     }
 }
